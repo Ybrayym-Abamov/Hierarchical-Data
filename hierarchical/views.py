11978de5fc39000256639c483089684d2dc67a09
Ybrayym <ybrayymjan90@gmail.com>
2020-06-24 11:44:05 -0400

    expalining what theview does

--- hierarchical/views.py
+++ hierarchical/views.py
@@ -3,6 +3,6 @@ from hierarchical.models import Hierarchy
 
 
 def hierarchical_data_view(request):
-    # data is everything
+    # data is everything from
     data = Hierarchy.objects.all()
     return render(request, 'main.html', {"data": data})
